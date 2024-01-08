50b314653090aee37fc1389bff71243f2a4bca4f
LinkedLst <sarang15mar@gmail.com>
2024-01-08 16:00:51 +0530

    adding main file

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Hotel Management APp",
+  description: "Find best hotels nearby",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+      <div className="font-normal">{children}</div>
+    </html>
+  );
+}
